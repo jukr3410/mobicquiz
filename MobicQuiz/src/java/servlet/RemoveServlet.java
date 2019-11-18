9a5a643ff2218eb3733007682859e0a9117d8772
deathmaner <57706145+deathmaner@users.noreply.github.com>
2019-11-19 00:01:47 +0700

    Update RemoveServlet.java

--- MobicQuiz/src/java/servlet/RemoveServlet.java
+++ MobicQuiz/src/java/servlet/RemoveServlet.java
@@ -16,6 +16,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import javax.transaction.UserTransaction;
 import jpacontroller.QuizsJpaController;
 import jpacontroller.exceptions.NonexistentEntityException;
@@ -43,7 +44,7 @@ public class RemoveServlet extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-       
+        HttpSession session = request.getSession();
        String remove = request.getParameter("remove");
        QuizsJpaController qjc = new QuizsJpaController(utx, emf); 
        
