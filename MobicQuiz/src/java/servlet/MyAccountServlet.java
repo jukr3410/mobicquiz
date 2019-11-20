175b712545bf5f71df79bb8e9d23a2f34fd5d393
deathmaner <57706145+deathmaner@users.noreply.github.com>
2019-11-20 18:28:09 +0700

    Update Change Password

--- MobicQuiz/src/java/servlet/MyAccountServlet.java
+++ MobicQuiz/src/java/servlet/MyAccountServlet.java
@@ -12,13 +12,24 @@ import java.io.PrintWriter;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.Resource;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.PersistenceUnit;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import javax.transaction.UserTransaction;
 import jpacontroller.HistorysJpaController;
 import jpacontroller.QuizsJpaController;
+import jpacontroller.StudentsJpaController;
+import jpacontroller.TeachersJpaController;
+import jpacontroller.exceptions.NonexistentEntityException;
+import jpacontroller.exceptions.RollbackFailureException;
 import model.ChangeImage;
 import model.Historys;
 import model.Quizs;
@@ -31,6 +42,12 @@ import org.apache.commons.io.FileUtils;
  * @author Student
  */
 public class MyAccountServlet extends HttpServlet {
+
+    @PersistenceUnit(unitName = "MobicQuizPU")
+    EntityManagerFactory emf;
+
+    @Resource
+    UserTransaction utx;
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -49,6 +66,62 @@ public class MyAccountServlet extends HttpServlet {
         String target = System.getProperty("user.home") + "/Documents/NetBeansProjects/mobicquiz/MobicQuiz/web/images/";
         String name = null;
 
+        String password = request.getParameter("password");
+        String newpassword = request.getParameter("newpassword");
+        String confirmnewpassword = request.getParameter("confirmnewpassword");
+
+        StudentsJpaController sjc = new StudentsJpaController(utx, emf);
+        TeachersJpaController tjc = new TeachersJpaController(utx, emf);
+
+        if (userType != null) {
+            if (userType.equals("student")) {
+                Students student = (Students) session.getAttribute("user");
+                if (password != null) {
+                    if (student.getPassword().equals(password) && newpassword.equals(confirmnewpassword)) {
+                        student.setPassword(newpassword);
+                        try {
+                            sjc.edit(student);
+                        } catch (NonexistentEntityException ex) {
+                            Logger.getLogger(MyAccountServlet.class.getName()).log(Level.SEVERE, null, ex);
+                        } catch (RollbackFailureException ex) {
+                            Logger.getLogger(MyAccountServlet.class.getName()).log(Level.SEVERE, null, ex);
+                        } catch (Exception ex) {
+                            Logger.getLogger(MyAccountServlet.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                        response.sendRedirect("/MobicQuiz/MyAccount");
+                        return;
+                    } else {
+                        request.setAttribute("error", "Can not reset password");
+                        request.getServletContext().getRequestDispatcher("/MyAccount.jsp").forward(request, response);
+                    }
+
+                }
+
+            } else if (userType.equals("teacher")) {
+                Teachers teacher = (Teachers) session.getAttribute("user");
+                if (password != null) {
+                    if (teacher.getPassword().equals(password) && newpassword.equals(confirmnewpassword)) {
+                        teacher.setPassword(newpassword);
+                        try {
+                            tjc.edit(teacher);
+                        } catch (NonexistentEntityException ex) {
+                            Logger.getLogger(MyAccountServlet.class.getName()).log(Level.SEVERE, null, ex);
+                        } catch (RollbackFailureException ex) {
+                            Logger.getLogger(MyAccountServlet.class.getName()).log(Level.SEVERE, null, ex);
+                        } catch (Exception ex) {
+                            Logger.getLogger(MyAccountServlet.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                        response.sendRedirect("/MobicQuiz/MyAccount");
+                        return;
+                    } else {
+                        request.setAttribute("error", "Can not reset password");
+                        request.getServletContext().getRequestDispatcher("/MyAccount.jsp").forward(request, response);
+                    }
+
+                }
+            }
+        }
+        
         if (userType != null) {
             if (userType.equals("student")) {
                 Students student = (Students) session.getAttribute("user");
@@ -69,22 +142,23 @@ public class MyAccountServlet extends HttpServlet {
             } catch (InterruptedException ex) {
                 Thread.currentThread().interrupt();
             }
-            
+
         }
 
-    }
+    
+}
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+// <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+/**
+ * Handles the HTTP <code>GET</code> method.
+ *
+ * @param request servlet request
+ * @param response servlet response
+ * @throws ServletException if a servlet-specific error occurs
+ * @throws IOException if an I/O error occurs
+ */
+@Override
+        protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         getServletContext().getRequestDispatcher("/MyAccount.jsp").forward(request, response);
     }
@@ -98,7 +172,7 @@ public class MyAccountServlet extends HttpServlet {
      * @throws IOException if an I/O error occurs
      */
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+        protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
@@ -109,7 +183,7 @@ public class MyAccountServlet extends HttpServlet {
      * @return a String containing servlet description
      */
     @Override
-    public String getServletInfo() {
+        public String getServletInfo() {
         return "Short description";
     }// </editor-fold>
 
