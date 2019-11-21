44325222772778c226f4a79e3f400ce68a9369d4
Student <Student@10.4.8.51>
2019-11-21 19:07:45 +0700

    update his

--- MobicQuiz/src/java/model/Historys.java
+++ MobicQuiz/src/java/model/Historys.java
@@ -33,8 +33,9 @@ import javax.xml.bind.annotation.XmlRootElement;
     @NamedQuery(name = "Historys.findAll", query = "SELECT h FROM Historys h")
     , @NamedQuery(name = "Historys.findByHistoryno", query = "SELECT h FROM Historys h WHERE h.historyno = :historyno")
     , @NamedQuery(name = "Historys.findByScore", query = "SELECT h FROM Historys h WHERE h.score = :score")
+    , @NamedQuery(name = "Historys.findByQuizno", query = "SELECT h FROM Historys h WHERE h.quizno = :quizno")
     , @NamedQuery(name = "Historys.findByStudentno", query = "SELECT h FROM Historys h WHERE h.studentno.studentno = :studentno")
-    , @NamedQuery(name = "Historys.findByTeacherno", query = "SELECT h FROM Historys h WHERE h.quizno.teacherno.teacherno = :teacherno")
+
     , @NamedQuery(name = "Historys.findByDate", query = "SELECT h FROM Historys h WHERE h.date = :date")})
 public class Historys implements Serializable {
 
@@ -49,12 +50,16 @@ public class Historys implements Serializable {
     @NotNull
     @Column(name = "SCORE")
     private int score;
+    @Basic(optional = false)
+    @NotNull
     @Column(name = "DATE")
     @Temporal(TemporalType.DATE)
     private Date date;
-    @JoinColumn(name = "QUIZNO", referencedColumnName = "QUIZNO")
-    @ManyToOne(optional = false)
-    private Quizs quizno;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 45)
+    @Column(name = "QUIZNO")
+    private String quizno;
     @JoinColumn(name = "STUDENTNO", referencedColumnName = "STUDENTNO")
     @ManyToOne(optional = false)
     private Students studentno;
@@ -71,7 +76,7 @@ public class Historys implements Serializable {
         this.score = score;
     }
 
-    public Historys(String historyno, int score, Date date, Quizs quizno, Students studentno) {
+    public Historys(String historyno, int score, Date date, String quizno, Students studentno) {
         this.historyno = historyno;
         this.score = score;
         this.date = date;
@@ -103,13 +108,15 @@ public class Historys implements Serializable {
         this.date = date;
     }
 
-    public Quizs getQuizno() {
+    public String getQuizno() {
         return quizno;
     }
 
-    public void setQuizno(Quizs quizno) {
+    public void setQuizno(String quizno) {
         this.quizno = quizno;
     }
+
+
 
     public Students getStudentno() {
         return studentno;
